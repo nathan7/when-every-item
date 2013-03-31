5bdb5269c66f1a026094188fc1015c4ed451fc40
Nathan Zadoks <nathan@nathan7.eu>
2013-03-31 05:42:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# when-every
+
+  Promise aggregation.
+
+## Installation
+
+    $ component install nathan7/when-every
+
+## API
+
+### whenEvery :: [Promise a] -> Promise [a]
+
+  Pretty much what it says on the box. Takes a bunch of promises, returns a promise for an array of all their results. Failure propagates.
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,18 @@
+var Promise = require('promise')
+module.exports = function whenEvery(promises) {
+  return new Promise(function(resolve, reject) {
+    if (!promises.length)
+      return resolve([])
+    var results = []
+      , done = 0
+    for (var i = 0, len = promises.length; i < len; i++)
+      promises[i].then(handler(i), reject)
+    function handler(i) {
+      return function(result) {
+        results[i] = result
+        if (++done === promises.length)
+          resolve(results)
+      }
+    }
+  })
+}
