7adecd9898503ed8b6689e3672346153302f91df
Nathan Zadoks <nathan@nathan7.eu>
2013-06-02 23:39:49 +0200

    unbreak everything

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ module.exports = function whenEveryItem(promises) {
     if (!promises.length)
       return resolve([])
     var len = promises.length
-      , results = Array(len)
+      , results = new Array(len)
       , done = 0
-    for (var i = 0, promise = promises[0]; i < len; promise = promises[++i]) {
-      if (!isPromise(promise))
+      , promise
+    for (var i = 0; i < len; i++) {
+      promise = promises[i]
+      if (isPromise(promise))
         promises[i].then(handler(i), reject)
       else {
         results[i] = promise
